297d0615e1c507f3dfc71fe6b06a73c46d78a22b
MerHS <starvessel@naver.com>
2020-02-03 23:08:47 +0900

    restart!

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,11 +1,11 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron'
 
 /**
  * Set `__statics` path to static files in production;
  * The reason we are setting it here is that the path needs to be evaluated at runtime
  */
 if (process.env.PROD) {
-  global.__statics = require('path').join(__dirname, 'statics').replace(/\\/g, '\\\\')
+  global.__statics = require('path').join(__dirname, 'statics').replace(/\\/g, '\\\\') // eslint-disable-line
 }
 
 let mainWindow
@@ -16,7 +16,7 @@ function createWindow () {
    */
   mainWindow = new BrowserWindow({
     width: 1000,
-    height: 600,
+    height: 1000,
     useContentSize: true,
     webPreferences: {
       nodeIntegration: true
@@ -43,3 +43,142 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        click(item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.webContents.send('undo');
+          }
+        },
+      },
+      { role: 'redo' },
+      { type: 'separator' },
+
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'delete' },
+      { role: 'selectall' },
+      { type: 'separator' },
+      {
+        label: 'Select Mode',
+        accelerator: 'F2',
+        click(item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.webContents.send('toSelectMode');
+          }
+        },
+      },
+      {
+        label: 'Write Mode',
+        accelerator: 'F3',
+        click(item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.webContents.send('toWriteMode');
+          }
+        },
+      },
+    ],
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' },
+    ],
+  },
+  {
+    role: 'window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'close' },
+    ],
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click() {
+          // eslint-disable-next-line 
+          require('electron').shell.openExternal('https://electron.atom.io');
+        },
+      },
+    ],
+  },
+];
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services', submenu: [] },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' },
+    ],
+  });
+
+  // Edit menu
+  template[1].submenu.push(
+    { type: 'separator' },
+    {
+      label: 'Speech',
+      submenu: [
+        { role: 'startspeaking' },
+        { role: 'stopspeaking' },
+      ],
+    },
+  );
+
+  // Window menu
+  template[3].submenu = [
+    { role: 'close' },
+    { role: 'minimize' },
+    { role: 'zoom' },
+    { type: 'separator' },
+    { role: 'front' },
+  ];
+}
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,28 +1,46 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR fFr">
+    <q-header elevated class="bg-primary text-white">
       <q-toolbar>
+        <q-btn
+          aria-label="Option"
+          dense
+          flat
+          icon="settings"
+          round
+        />
+
+        <q-toolbar-title>
+          SARISSA - Rhythmin Editor
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+
         <q-btn
           aria-label="Menu"
           dense
           flat
           icon="menu"
           round
-          @click="store.toggleLeftDrawer()"
+          class="q-ml-xs"
+          @click="store.toggleRightDrawer()"
         />
-
-        <q-toolbar-title>
-          Quasar - Clean Typescript
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
+      v-model="rightDrawerOpen"
+      :show-if-above="false"
+      side="right"
+      overlay 
+      behavior="desktop"
+      elevated
       content-class="bg-grey-2"
     >
       <q-list>
@@ -36,82 +54,21 @@
             <q-item-label caption>quasar.dev</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item
-          clickable
-          href="https://github.quasar.dev"
-          tag="a"
-          target="_blank"
-        >
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          href="https://chat.quasar.dev"
-          tag="a"
-          target="_blank"
-        >
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          href="https://forum.quasar.dev"
-          tag="a"
-          target="_blank"
-        >
-          <q-item-section avatar>
-            <q-icon name="record_voice_over" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          href="https://twitter.quasar.dev"
-          tag="a"
-          target="_blank"
-        >
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          href="https://facebook.quasar.dev"
-          tag="a"
-          target="_blank"
-        >
-          <q-item-section avatar>
-            <q-icon name="public" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Facebook</q-item-label>
-            <q-item-label caption>@QuasarFramework</q-item-label>
-          </q-item-section>
-        </q-item>
+        
       </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer bordered class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-icon :name="editModeIcon"/>
+        <q-space></q-space>
+        &copy; 2020 KINETC
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -128,12 +85,16 @@ import LayoutStoreModule from './LayoutStoreModule';
 export default class MyLayout extends Vue {
   store = getModule(LayoutStoreModule);
 
-  get leftDrawerOpen() {
-    return this.store.leftDrawerOpen;
+  get rightDrawerOpen() {
+    return this.store.rightDrawerOpen;
   }
 
-  set leftDrawerOpen(value: boolean) {
-    this.store.setLeftDrawerOpen(value);
+  set rightDrawerOpen(value: boolean) {
+    this.store.setRightDrawerOpen(value);
+  }
+
+  get editModeIcon(): string {
+    return 'edit';
   }
 }
 </script>
